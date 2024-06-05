4380c376ca94a950d391a283d4182cac340d2e46
Daniel <danielpietschke@gmx.de>
2024-06-05 14:28:56 +0300

    initial commit

---
+++ project/minesweeper.py
@@ -0,0 +1,15 @@
+import random 
+
+# Printing Layout
+
+def print_mines_layout():
+    global mine_values
+    global n
+
+    print(n, mine_values)
+
+
+n= 5
+mine_values = 4
+
+print_mines_layout()
